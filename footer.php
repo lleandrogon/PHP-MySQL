97a972a68a9656ffe7b8428f211f1497488f33ee
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-01-28 21:37:48 -0300

    Primeiro commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+
+<footer>
+    <div class="container">
+        ©2018 DogCursos - Todos os direitos reservados 
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DogCursos</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img src="img/logo.png" title="Logo" alt="Logo">
+            <div id="menu">
+                <a href="#">Cursos</a>
+                <a href="#">Alunos</a>
+                <a href="#">Matrículas</a>
+            </div>
+        </div>
+    </header>
+
+    <div id="conteudo" class="container">
+</body>
+</html>
