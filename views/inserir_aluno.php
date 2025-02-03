76ddd80fdf7e5a68223491a40782b3786db60c80
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-02-02 21:58:36 -0300

    Terminando funcionalidades do CRUD

--- views/inserir_aluno.php
+++ views/inserir_aluno.php
@@ -1,3 +1,7 @@
+<?php 
+    if (!isset($_GET["editar"])) {
+?>
+
 <h1>Inserir novo aluno</h1>
 <form method="POST" action="processa_aluno.php">
     <br>
@@ -13,3 +17,40 @@
     <br>
     <input type="submit" value="Inserir aluno">
 </form>
+
+<?php 
+    } else {
+?>
+<?php 
+     while ($linha = mysqli_fetch_array($consulta_alunos)) {
+?>
+<?php 
+    if ($linha["id_aluno"] == $_GET["editar"]) {
+?>
+
+<h1>Editar aluno</h1>
+<form method="POST" action="edita_aluno.php">
+    <input type="hidden" name="id_aluno" value="<?php echo $linha["id_aluno"]; ?>">
+    <br>
+    <label>Nome aluno:</label>
+    <br>
+    <input type="text" name="nome_aluno" value="<?php echo $linha["nome_aluno"]; ?>" placeholder="Insira o nome do aluno">
+    <br>
+    <br>
+    <label>Data de nascimento</label>
+    <br>
+    <input type="text" name="data_nascimento" value="<?php echo $linha["data_nascimento"]; ?>" placeholder="Insira a data de nascimento">
+    <br>
+    <br>
+    <input type="submit" value="Editar aluno">
+</form>
+
+<?php 
+    }
+?>
+<?php 
+     }
+?>
+<?php 
+    }
+?>
--- views/inserir_curso.php
+++ views/inserir_curso.php
@@ -1,3 +1,6 @@
+<?php 
+    if (!isset($_GET["editar"])) {
+?>
 <h1>Inserir novo curso</h1>
 <form method="POST" action="processa_curso.php">
     <br>
@@ -13,3 +16,40 @@
     <br>
     <input type="submit" value="Inserir curso">
 </form>
+
+<?php 
+    } else {
+?>
+<?php 
+     while ($linha = mysqli_fetch_array($consulta_cursos)) {
+?>
+<?php 
+    if ($linha["id_curso"] == $_GET["editar"]) {
+?>
+
+<h1>Editar curso</h1>
+<form method="POST" action="edita_curso.php">
+    <input type="hidden" name="id_curso" value="<?php echo $linha["id_curso"]; ?>">
+    <br>
+    <label>Nome curso</label>
+    <br>
+    <input type="text" name="nome_curso" placeholder="Insira o nome do curso" value="<?php echo $linha["nome_curso"]; ?>">
+    <br>
+    <br>
+    <label>Carga horária</label>
+    <br>
+    <input type="text" name="carga_horaria" value="<?php echo $linha["carga_horaria"]; ?>" placeholder="Insira a carga horária">
+    <br>
+    <br>
+    <input type="submit" value="Editar curso">
+</form>
+
+<?php 
+    }
+?>
+<?php 
+     }
+?>
+<?php 
+    }
+?>
